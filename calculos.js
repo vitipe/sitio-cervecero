98cb1107a08cae299985e299ef94f537a2f0b0a9
vitipe <victorcasbas@gmail.com>
2020-12-15 09:57:23 -0300

    Agregado de cákculo ABV
    Calculo de ABV de cerveza y plantilla de las próximas fórmulas a completar

--- calculos.js
+++ calculos.js
@@ -1,9 +1,21 @@
 /*
 Acá ir agregando las formulas y calculos para lo que sería la página
+
+Ver de robarlas de "How to brew" o "Designing great beers"
 */
 
 function calculoABV (OG, FG) {
     let abvBirra = (OG - FG * 131.25)
     return abvBirra
-}
+};
 
+
+//Formula para corrección densímetro
+
+//Formula IBUs
+
+//Formula balance?
+
+//Mash calculator
+
+//Dilution and boil off
